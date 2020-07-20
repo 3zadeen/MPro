736ed5ed3710b6a8cf126dd32c2637b626f34294
Ezaden <ezadens@yahoo.com>
2020-07-20 21:54:13 +0800

    Initial Commit

---
+++ MPro/MProApp.swift
@@ -0,0 +1,17 @@
+//
+//  MProApp.swift
+//  MPro
+//
+//  Created by Ezaden Seraj on 20/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct MProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
